529ec38b1ca7191739c92c000a6d3b95a247a5f1
ANKIT KARN <45123508+AnkitKarnAK@users.noreply.github.com>
2018-11-27 23:45:36 +0530

    Update final_test.py

--- final_test.py
+++ final_test.py
@@ -6,7 +6,7 @@ import time
 import os
 
 
-client = discord.Client()
+client =  commands.Bot(command_prefix=';')
 
 @client.event
 async def on_ready():
@@ -21,10 +21,24 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('ping'):
        await client.send_message(message.channel, 'Pong!<:pepePunch:516721734607699968>')
-
     if message.content=="hi":
         await client.send_message(message.channel,"{0.author.mention} hye there :tada:<:blobcookie:516669559906893825>".format(message))
 
+    await client.process_commands(message)
+    
+@client.command()
+async def echo(*args):
+    output =' '
+    for word in args:
+        output +=word
+        output +=' '
+    await client.say(output)
+    
+@client.command(pass_context=True)
+async def ping(ctx):
+    t = await client.say('Calculating Heartbeat')
+    ms = (t.timestamp-ctx.message.timestamp).total_seconds() * 1000
+    await client.edit_message(t, new_content='Pong!<:pepePunch:516721734607699968>: my heartbeat is: {}ms:heartpulse:'.format(int(ms)))
 
 
 client.run(os.getenv('TOKEN'))
